48eb777f1b0123a483820c0d816fb18fc9346b25
Wiliam Traoré <hi@wil-liam.com>
2024-02-11 19:12:08 +0100

    chore: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	output: 'server',
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare()
 });
