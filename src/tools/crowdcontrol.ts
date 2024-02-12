fca5ac825762cd62a3dd8e3baa7ef83553c34172
tainalo2 <rongier.alexandre@outlook.fr>
2024-02-12 14:15:51 +0100

    Add Aitum, Aitum Vertical, CrowdControl, str.bot

---
+++ src/tools/crowdcontrol.ts
@@ -0,0 +1,20 @@
+import type { Tool } from '../types/tool';
+
+const tool: Tool = {
+	iconUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/f18ee763-81a1-4a9e-9a53-793e5bf195a4-profile_image-300x300.png',
+	name: 'CrowdControl',
+	slug: 'crowdcontrol',
+	url: 'https://crowdcontrol.live/',
+	urlDisplay: 'crowdcontrol.live',
+	description:
+		'Software that permit viewers to interact directly in your game, spawn mob, transform world, and more.',
+	prices: ['free', 'paid'],
+	providers: ['twitch', 'youtube', 'kick', 'tiktok'],
+	tags: ['chat', 'interactions', 'software'],
+	screenshots: [
+		'https://images.squarespace-cdn.com/content/v1/6111b81345fb3406c893d1c7/c60f8966-1aac-4ae0-9e05-3f6b1d7438df/StreamAnywhere+%281%29.png',
+		'https://crowdcontrol.live/wp-content/uploads/2023/05/blog-15-1536x864.png',
+	],
+};
+
+export default tool;
---
+++ src/tools/streamerDotBot.ts
@@ -0,0 +1,20 @@
+import type { Tool } from '../types/tool';
+
+const tool: Tool = {
+	iconUrl: 'https://wiki.streamer.bot/logos/streamerbot.png',
+	name: 'streamer.bot',
+	slug: 'streamer.bot',
+	url: 'https://streamer.bot/',
+	urlDisplay: 'streamer.bot',
+	description:
+		'Software to setup interactions and automations scripts over lot of services from Twitch and YT, to Lumia, elgato, VoiceMod and many more.',
+	prices: ['free'],
+	providers: ['twitch', 'youtube'],
+	tags: ['automation', 'interactions', 'scripts', 'software'],
+	screenshots: [
+		'https://i.ibb.co/48Tz115/Capture-d-cran-2024-02-12-133455.png',
+		'https://i.ibb.co/CnHbF4G/Capture-d-cran-2024-02-12-133551.png',
+	],
+};
+
+export default tool;
