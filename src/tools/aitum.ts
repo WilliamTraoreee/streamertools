50baf431575295a317bc2b2dad18cc9220223a8a
tainalo2 <rongier.alexandre@outlook.fr>
2024-02-12 14:15:51 +0100

    Add Aitum, Aitum Vertical, CrowdControl, str.bot

---
+++ src/tools/aitum.ts
@@ -0,0 +1,20 @@
+import type { Tool } from '../types/tool';
+
+const tool: Tool = {
+	iconUrl: 'https://pbs.twimg.com/profile_images/1662126076317908992/erKe1tYV_400x400.png',
+	name: 'Aitum',
+	slug: 'aitum',
+	url: 'https://aitum.tv/',
+	urlDisplay: 'aitum.tv',
+	description:
+		'Software to create interactions and scenarios with viewers. Very good UI. Multi-PC, MIDI, OSC, and more.',
+	prices: ['paid'],
+	providers: ['twitch', 'youtube', 'tiktok'],
+	tags: ['interactions', 'scripts', 'midi', 'OSC', 'software'],
+	screenshots: [
+		'https://img.youtube.com/vi/7Q_sJ-SwJ90/maxresdefault.jpg',
+		'https://img.youtube.com/vi/H3kgzXjT6cA/maxresdefault.jpg',
+	],
+};
+
+export default tool;
