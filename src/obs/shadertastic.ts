0e2744e846c29fb88de84d6ef16b12301473f35f
tainalo2 <rongier.alexandre@outlook.fr>
2024-02-13 14:28:24 +0100

    Add obs folder, face-tracking.ts, mov shadertastic

--- src/obs/shadertastic.ts
+++ src/obs/shadertastic.ts
@@ -12,8 +12,6 @@ const tool: Tool = {
 	providers: [],
 	tags: ['OBS', 'plugin', 'shader'],
 	screenshots: [
-		'https://i.ibb.co/nz74bgf/Capture-d-cran-2024-02-13-093134.png',
-		'https://i.ibb.co/YXjSqyq/Capture-d-cran-2024-02-13-093216.png',
 	],
 };
 
