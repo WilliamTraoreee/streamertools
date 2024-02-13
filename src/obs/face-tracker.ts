f953f26d793f7ff5c6a147848e48d111734a0aae
tainalo2 <rongier.alexandre@outlook.fr>
2024-02-13 14:28:24 +0100

    Add obs folder, face-tracking.ts, mov shadertastic

---
+++ src/obs/face-tracker.ts
@@ -0,0 +1,20 @@
+import type { Tool } from '../types/tool';
+
+const tool: Tool = {
+	iconUrl: 'https://avatars.githubusercontent.com/u/109690726?s=200&v=4',
+	name: 'Face Tracker',
+	slug: 'face-tracker',
+	url: 'https://github.com/norihiro/obs-face-tracker',
+	urlDisplay: 'github.com/norihiro/obs-face-tracker',
+	description:
+		'OBS plugin using CPU to track your face. Good to use in zoom or crop camera.',
+	prices: ['free'],
+	providers: [],
+	tags: ['OBS', 'plugin'],
+	screenshots: [
+		'https://i.ibb.co/qjY1N51/Capture-d-e-cran-2024-02-11-a-18-24-03.png',
+		'https://i.ibb.co/Bw2gKb1/Capture-d-e-cran-2024-02-11-a-18-24-39.png',
+	],
+};
+
+export default tool;
