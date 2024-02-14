017b3f120f284d4962b9342a5d4a3d063928b4b6
Wiliam Traoré <hi@wil-liam.com>
2024-02-14 20:29:31 +0100

    chore: generate sitempa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
-
 import preact from '@astrojs/preact';
+import fs from 'node:fs';
+
+const getToolsSitemap = () => {
+	const toolsUrl = 'https://streamerstools.app/tools';
+	const allTools = fs.readdirSync('./src/tools/');
+	return allTools
+		? allTools.map((tool) => `${toolsUrl}/${tool.split('.')[0]}`)
+		: [];
+};
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://streamertools.app',
 	integrations: [
 		tailwind(),
-		sitemap(),
+		sitemap({
+			customPages: getToolsSitemap(),
+		}),
 		preact({
 			compat: true,
 		}),
