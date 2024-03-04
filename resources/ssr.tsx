46042fb0ef0024751db3af27f945c35e848c92d6
Wiliam Traoré <hi@wil-liam.com>
2024-03-04 12:08:36 +0100

    fix: ssr

--- resources/ssr.tsx
+++ resources/ssr.tsx
@@ -1,14 +1,23 @@
 import { createInertiaApp } from '@inertiajs/react'
 import ReactDOMServer from 'react-dom/server'
+import { Layout } from './components/layout/layout'
 
 export default function render(page: any) {
   return createInertiaApp({
     page,
     render: ReactDOMServer.renderToString,
-    resolve: name => {
-      const pages = import.meta.glob('./pages/**/*.tsx', { eager: true })
-      return pages[`./pages/${name}.tsx`]
+    resolve: (name) => {
+      const pages = import.meta.glob('./pages/**/*.tsx')
+      return pages[`./pages/${name}.tsx`]()
     },
-    setup: ({ App, props }) => <App {...props} />,
+    setup: ({ App, props }) => {
+      const user = props.initialPage.props.user
+
+      return (
+        <Layout user={user}>
+          <App {...props} />
+        </Layout>
+      )
+    },
   })
 }
